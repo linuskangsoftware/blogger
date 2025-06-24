302dad1c7381e9a400b92401df869ee4042aa867
linuskang <linuskang09@gmail.com>
2025-06-24 17:43:32 +1000

    Fix up fetchBlogDetails: error because of api token change

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-
-
 const url = process.env.BLOG_URL || "http://localhost:3000"
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,8 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export async function fetchBlogDetails() {
+  const api_token = process.env.API_TOKEN
+
   const res = await fetch(`${url}/api`, {
     cache: "no-store",
+    headers: {
+      "Authorization": `Bearer ${api_token}`
+    },
   })
   if (!res.ok) throw new Error("Failed to fetch")
   return res.json()
